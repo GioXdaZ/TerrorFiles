dbb569c7c3636a16af86eb69c6bcf6067c0c872d
GioXdaZ <gio.daz@gmail.com>
2022-11-03 21:46:26 -0300

    1° Commit

---
+++ src/App.jsx
@@ -0,0 +1,139 @@
+import "./App.css";
+import { Carousel } from "./components/Carousel";
+
+function App() {
+  return (
+    <div className="body bg-slate-900">
+      <div className="navbar bg-base-100 fixed z-10">
+        <div className="flex-1 justify-center">
+          <a className="normal-case text-xl">TerrorFiles</a>
+        </div>
+      </div>
+
+      <Carousel></Carousel>
+
+      <div className="middle-body p-2 mt-8">
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-body text-center">Copyright by GioXdaZ© 2022</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,36 @@
+import { motion } from "framer-motion";
+import imgs from "./images";
+import "./Carousel.css";
+import { useRef, useEffect, useState } from "react";
+
+export const Carousel = () => {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
+  return (
+    <motion.div
+      ref={carousel}
+      className="carousel"
+      whileTap={{ cursor: "grabbing" }}
+    >
+      <motion.div
+        drag="x"
+        dragConstraints={{ right: 0, left: -width }}
+        className="inner-carousel"
+      >
+        {imgs.map((img) => {
+          return (
+            <motion.div className="item" key={img}>
+              <img src={img} alt="" />
+            </motion.div>
+          );
+        })}
+      </motion.div>
+    </motion.div>
+  );
+};
